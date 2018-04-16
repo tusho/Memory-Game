aaec61fdf217829a1e70d2455b58b9e891000797
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-04-16 11:54:19 +1000

    pulled DOM elements and placed them into an array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -15,3 +14,27 @@ function shuffle(array) {
 
     return array;
 }
+
+// var cardArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+// var cardArray = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// var cardArray = [ul.children]
+// cardArray = shuffle(cardArray);
+// console.log(cardArray);
+
+const iElements = document.querySelector('.deck');
+const iElementClasses = iElements.querySelectorAll('li');
+const iElementClassMates = iElements.children;
+let myArray = [];
+
+
+for (let i = 0; i < iElementClasses.length; i++) {
+  myArray[i] = (iElementClasses[i].innerHTML);
+}
+
+myArray = myArray.map(el => el.trim());
+
+
+
+// for (let j = 0; j < iElementClassMates.length; j++) {
+//   console.dir(iElementClassMates[j].innerHTML);
+// }
