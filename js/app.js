7363696d8e610206ce99731b9858d5c649a1353b
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-04-16 13:38:23 +1000

    simplified code and added a function to shuffle the css classes

--- js/app.js
+++ js/app.js
@@ -15,26 +15,17 @@ function shuffle(array) {
     return array;
 }
 
-// var cardArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
-// var cardArray = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-// var cardArray = [ul.children]
-// cardArray = shuffle(cardArray);
-// console.log(cardArray);
-
-const iElements = document.querySelector('.deck');
-const iElementClasses = iElements.querySelectorAll('li');
-const iElementClassMates = iElements.children;
+//Pull cards into an Array
 let myArray = [];
-
-
-for (let i = 0; i < iElementClasses.length; i++) {
-  myArray[i] = (iElementClasses[i].innerHTML);
+var myCards = document.getElementById('deck').getElementsByTagName('i');
+for (let i = 0; i < myCards.length; i++) {
+  myArray[i] = (myCards[i].className);
 }
 
-myArray = myArray.map(el => el.trim());
-
-
-
-// for (let j = 0; j < iElementClassMates.length; j++) {
-//   console.dir(iElementClassMates[j].innerHTML);
-// }
+//Function to change the deck my exchanging CSS classes
+function changeDecks() {
+  shuffle(myArray);
+  for (let j=0; j < myCards.length; j++) {
+    myCards[j].className = myArray[j];
+  }
+}
